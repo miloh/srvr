807d04b26025f35f5fa3824b9261cef730d9be0a
r. miloh alexander <miloh@froggytoad.net>
2026-04-26 08:46:16 -0700

    Add device models

--- src/frontend/pages/mod.rs
+++ src/frontend/pages/mod.rs
@@ -5,6 +5,7 @@ mod setup;
 mod template_editor;
 mod templates;
 mod users;
+mod device_models;
 
 pub use dashboard::Dashboard;
 pub use devices::{DeviceDetail, Devices};
@@ -13,3 +14,4 @@ pub use setup::Setup;
 pub use template_editor::TemplateEditor;
 pub use templates::Templates;
 pub use users::Users;
+pub use device_models::	DeviceModels;
